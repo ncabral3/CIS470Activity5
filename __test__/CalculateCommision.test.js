098a37bb585a408e851cc3ab69eff407c51cef59
Nick Cabral <145383553+ncabral3@users.noreply.github.com>
2024-03-06 18:11:11 -0500

    act5_5

--- __test__/CalculateCommision.test.js
+++ __test__/CalculateCommision.test.js
@@ -1,11 +1,11 @@
-import calculateCommissionFunction from "./CalculateComission";
+const calculateCommissionFunction = require("../CalculateCommission.js");
  
 describe("Calculate Commission", () => {
-    test("Calculate commission for 70 locks, 80 stocks, and 90 barrels", () => {
-        expect(calculateCommissionFunction(35, 40, 45)).toEqual([3675, 735]);
+    test("Calculate commission for 35 locks, 40 stocks, and 45 barrels", () => {
+        expect(calculateCommissionFunction(35, 40, 45)).toEqual([3900, 771]);
     });
     test("Calculate commission for 70 locks, 80 stocks, and 90 barrels", () => {
-        expect(calculateCommissionFunction(70, 80, 90)).toEqual([7350, 1470]);
+        expect(calculateCommissionFunction(70, 80, 90)).toEqual([7800, 1551]);
     });
 
     test("Calculate commission for 71 locks, 80 stocks, and 90 barrels", () => {
